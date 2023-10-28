28d7e8961af5fb8c8fee57aeba28c150eac1ac48
Hudzaifahlilop <106752278+Hudzaifahlilop@users.noreply.github.com>
2023-10-28 15:06:09 +0700

    Update app.py tes

--- app.py
+++ app.py
@@ -55,6 +55,8 @@ def get_conversation_chain(vectorstore):
 
 def main():
     load_dotenv()
+    openai_api_key = os.environ.get('OPENAI_API_KEY')
+    st.write(open_api_key)
 
     st.set_page_config(page_title='Talking PDF')
 
